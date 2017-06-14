a55fc2ec2c3df6ae7ab9bbd5c677be5c9fd148e6
Quincy Delp <quincy@dyn-160-39-8-215.dyn.columbia.edu>
2017-06-14 13:55:28 -0400

    full UI update, preliminary content, some LIPSM, no outside buttons functional

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+
+
+(function($) {
+
+	skel.breakpoints({
+		wide: '(max-width: 1680px)',
+		normal: '(max-width: 1280px)',
+		narrow: '(max-width: 1000px)',
+		mobile: '(max-width: 736px)'
+	});
+
+
+	//modal stuff 
+
+	// Get the modal
+	var modal = document.getElementById('myModal');
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks the button, open the modal 
+	btn.onclick = function() {
+	    modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	}
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// CSS polyfills (IE<9).
+			if (skel.vars.IEVersion < 9)
+				$(':last-child').addClass('last-child');
+
+		// Scrolly links.
+			$('.scrolly').scrolly();
+
+		// Prioritize "important" elements on narrow.
+			skel.on('+narrow -narrow', function() {
+				$.prioritize(
+					'.important\\28 narrow\\29',
+					skel.breakpoint('narrow').active
+				);
+			});
+
+	});
+
+})(jQuery);
